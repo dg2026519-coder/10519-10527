5c60592452d7b180c67e01e8a2f75268037e4946
dg2026519-coder <dg2026519@danggok.hs.kr>
2026-05-26 12:10:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+5/26
 
+ground = box(pos=vector(0, -5, 0), size=vector(10, 0.2, 4), color=color.blue)
+b= box(pos=vector(0, 4, 0), radius=0.3,)
+v = vector(0, 0, 0)
+g = vector(0, -9.8, 0)
+dt = 0.02
+a = arrow(size = vec(5,5,5), pos = vec (0,10,0),color= color.cyan, axis = vec(0,-1,0))
+
+while b.pos.y > -4.5:
+    rate(60)
+    
+
+    k = keysdown()
+    if 'down' in k :
+        a.axis.y -= 0.5
+    if ' ' in k :
+        v = v + g * dt
+        b.pos = b.pos + v * dt
