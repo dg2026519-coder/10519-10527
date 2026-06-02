a119b1dddeeff6279fd1c8cb03c064481c299b47
dg2026519-coder <dg2026519@danggok.hs.kr>
2026-06-02 12:08:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-Web VPython 3.2
 ground = box(pos=vector(0, -5, 0), size=vector(10, 0.2, 4), color=color.blue)
+
+    
+v = vector(0, 0, 0)
+g = vector(0, -9.8, 0)
+dt = 0.02
+
+  
 b= box(pos=vector(0, 4, 0), radius=0.3,)
 v = vector(3.0, 0, 0)
 dt = 0.02
@@ -11,22 +17,6 @@ while t < 8:
     if b.pos.x > 5.6 or b.pos.x < -5.6:
         v = vector(-v.x, v.y, v.z)
     t = t + dt
-    
-v = vector(0, 0, 0)
-g = vector(0, -9.8, 0)
-dt = 0.02
-a = arrow(size = vec(5,5,5), pos = vec (0,10,0),color= color.cyan, axis = vec(0,-1,0))
-v = vector(3.0, 0, 0)
-dt = 0.02
-
-t = 0
-while t < 8:
-    rate(60)
-    a.pos = a.pos + v * dt
-    if a.pos.x > 5.6 or a.pos.x < -5.6:
-        v = vector(-v.x, v.y, v.z)
-    t = t + dt
-    
     
     
     
