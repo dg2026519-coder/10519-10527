9e865943be259e4b6f0f453adad77c5e5511fbe7
dg2026519-coder <dg2026519@danggok.hs.kr>
2026-06-09 12:09:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
-Web VPython 3.2
-ground = box(pos=vector(0, -5, 0), size=vector(10, 0.2, 4), color=color.green)
 
-r = ring(pos=vector(3.7, -4, 0), size=vector(0.2, 2,2, 2,2), color=color.white, axis = vec(0,1,0))
+
+Web VPython 3.2
+ground = box(pos=vector(3, -5, 0), size=vector(5, 0.2, 2), color=color.green)
+
+r = ring(pos=vector(3.7, -4, 0), size=vector(0.1, 1,1, 1,1), color=color.white, axis = vec(0,1,0))
 
 v = vector(0, 0, 0)
 g = vector(0, -9.8, 0)
 dt = 0.02
 
   
-b= box(pos=vector(0, 4, 0), radius=0.3)
+s= sphere(pos=vector(0, 4, 0),radius=0.3)
 v = vector(3.0, 0, 0)
 dt = 0.02
 
 t = 0
-while t < 8 and b.pos.y < -4.5 :
+while t < 8 and s.pos.y < -4.5 :
     rate(60)
-    b.pos = b.pos + v * dt
-    if b.pos.x > 5.6 or b.pos.x < -5.6:
+    s.pos = s.pos + v * dt
+    if s.pos.x > 5.6 or s.pos.x < -5.6:
         v = vector(-v.x, v.y, v.z)
     t = t + dt
     
     
     
-while b.pos.y > -4.5:
+while s.pos.y > -4.5:
     rate(60)
     
 
@@ -31,7 +33,7 @@ while b.pos.y > -4.5:
         a.axis.y -= 0.5
     if ' ' in k :
         v = v + g * dt
-        b.pos = b.pos + v * dt
+        s.pos = s.pos + v * dt
 
 
 
