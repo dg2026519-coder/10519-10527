83b24728d6b17bc893d73755774c9d903c30b8d0
dg2026519-coder <dg2026519@danggok.hs.kr>
2026-06-09 11:30:40 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ g = vector(0, -9.8, 0)
 dt = 0.02
 
   
-b= box(pos=vector(0, 4, 0), radius=0.3,)
+b= box(pos=vector(0, 4, 0), radius=0.3)
 v = vector(3.0, 0, 0)
 dt = 0.02
 
 t = 0
-while t < 8 and pos.y < -4.5
+while t < 8 and b.pos.y < -4.5 :
     rate(60)
     b.pos = b.pos + v * dt
     if b.pos.x > 5.6 or b.pos.x < -5.6:
@@ -31,6 +31,7 @@ while b.pos.y > -4.5:
     if ' ' in k :
         v = v + g * dt
         b.pos = b.pos + v * dt
+
 
 
 
